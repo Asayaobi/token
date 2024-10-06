78e962af58a9619cd1c3873bcf13dcbb89346f02
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-10-06 17:30:26 +0200

    update balance result

--- src/token_assets/src/components/Balance.jsx
+++ src/token_assets/src/components/Balance.jsx
@@ -5,12 +5,19 @@ import {token} from "../../../declarations/token"
 function Balance() {
 
   const [inputValue, setInput] = useState("")
+  const [balanceResult, setBalance] = useState("")
   
   async function handleClick() {
-    console.log(inputValue);
+    // console.log(inputValue);
+    //1. call balanceOf() from backend token canister
     //convert inputValue as Principal type in order to use with balanceOf()
     const principal = Principal.fromText(inputValue)
     const balance = await token.balanceOf(principal)
+
+    //2. display the result as balanceResult
+    //the balance from backend is passed as Natural number format eg. 1_000_000_000
+    //we have to convert it to the string with toLocaleString method
+    setBalance(balance.toLocaleString())
   }
 
 
@@ -35,7 +42,7 @@ function Balance() {
           Check Balance
         </button>
       </p>
-      <p>This account has a balance of XYZ.</p>
+      <p>This account has a balance of {balanceResult}.</p>
     </div>
   );
 }
