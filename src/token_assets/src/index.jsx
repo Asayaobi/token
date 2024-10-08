afd7ee3745cb4693557979153b7397630d084729
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-10-08 20:03:04 +0200

    add principal id

--- src/token_assets/src/index.jsx
+++ src/token_assets/src/index.jsx
@@ -1,11 +1,40 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
-import App from "./components/App";
+import App from "./components/App"
+import { AuthClient } from '../../../node_modules/@dfinity/auth-client/lib/cjs/index'
+
 
 const init = async () => { 
-  ReactDOM.render(<App />, document.getElementById("root"));
+  const authClient = await AuthClient.create()
+  
+  //check if the user has already logged in before
+  if (await authClient.isAuthenticated()) {
+    handleAuthenticated(authClient)
+  } else {
+    //if the user has never logged in before
+     await authClient.login({
+    identityProvider: "https://identity.ic0.app/#authorize",
+    //once it's successfully authenticated, take the user to the main app
+    onSuccess: () => {
+      handleAuthenticated(authClient)
+    }
+  })
+  }
 }
 
-init();
+async function handleAuthenticated(authClient) {
+  //log authClient in the frontend
+  console.log(authClient.getIdentity())
+
+  //get user's principal from getIdentity()
+  const identity = await authClient.getIdentity()
+  const userPrincipal = identity._principal.toString()
+  console.log(userPrincipal)
+  
+  //render the website
+  ReactDOM.render(<App />, document.getElementById("root"))
+}
+
+init()
 
 
